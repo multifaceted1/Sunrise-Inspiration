17ea502d9ac99da071f655960bcd1762ba5b1e6e
Margaret Kellogg <imgonnaletitshine@hotmail.com>
2020-12-28 11:46:31 -0500

    improving js file and adding quotes

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var quoteArray = [
     "Every great dream begins with a dreamer. Always remember, you have within you the strength, the patience, and the passion to reach for the stars to change the world. ~ Harriet Tubman",
     "I had reasoned this out in my mind, there was one of two things I had a right to, liberty or death; if I could not have one, I would have the other. ~ Harriet Tubman",
     "Power concedes nothing without a demand. It never did and it never will. ~ Frederick Douglas",
+    "Let the waters settle and you will see the moon and the stars mirrored in your own being. ~ Rumi",
     "A smile or a tear has no nationality; joy and sorrow speak alike to all nations, and they, above all the confusion of tongues, proclaim the brotherhood of man. ~ Frederick Douglas",
     "The best way to find yourself is to lose yourself in the service of others. ~ Mahatma Ghandi",
     "I object to violence because when it appears to do good, the good is only temporary; the evil it does is permanent. ~ Mahatma Ghandi",
@@ -13,7 +14,13 @@ var quoteArray = [
     "Nonviolence is a powerful and just weapon, which cuts without wounding and ennobles the man who wields it. It is a sword that heals. ~ Rev. Dr. Martin Luther King Jr.",
     "Never do things others can do and will do, if there are things others cannot do or will not do. ~ Amelia Earhart",
     "It is no use saying ‘we are doing our best.’ You have got to succeed in doing what is necessary. ~ Winston Churchill",
+    "The universe is not outside of you. Look inside yourself; everything that you want, you already are. ~ Rumi",
     "When the power of love overcomes the love of power the world will know peace. ~ Jimi Hendrix",
+    "Stop acting so small. You are the universe in ecstatic motion. ~ Rumi",
+    "Spread love everywhere you go. ~ Mother Teresa",
+    "Most ‘impossible’ goals can be met simply by breaking them down into bite size chunks, writing them down, believing them, and then going full speed ahead as if they were routine. ~ Don Lancaster",
+    "Liberty! Freedom! Democracy! True anyhow, no matter how many liars use those words. ~ Langston Hughes",
+    "Love one another and help others to rise to the higher levels, simply by pouring out love. Love is infectious and the greatest healing energy. - Sai Baba",
     "We do not have to become heroes overnight. Just a step at a time, meeting each thing that comes up, seeing it as not as dreadful as it appears, discovering that we have the strength to stare it down. ~ Eleanor Roosevelt"
 ];
 
@@ -21,7 +28,12 @@ $("#inspired").click(function(){
 
 $(".inspiredQuote").show();
 randomQuoteDisplay = quoteArray[Math.floor(Math.random() * quoteArray.length)];
-$(".randomQuote").text(randomQuoteDisplay)});
+$(".randomQuote").text(randomQuoteDisplay);
+$(".energeticAdvice").hide();
+$(".productiveTips").hide();
+$(".appreciativeFacts").hide();
+$(".feelRelaxed").hide();
+});
 
 $("#energetic").click(function(){
     $(".energeticAdvice").show();
@@ -29,6 +41,7 @@ $("#energetic").click(function(){
     $(".appreciativeFacts").hide();
     $(".feelRelaxed").hide();
     $(".inspiredQuote").hide();
+    $(".randomQuote").text("");
 });
 
 $("#relaxed").click(function(){
@@ -37,6 +50,7 @@ $("#relaxed").click(function(){
     $(".appreciativeFacts").hide();
     $(".energeticAdvice").hide();
     $(".inspiredQuote").hide();
+    $(".randomQuote").text("");
 })
 
 $("#appreciative").click(function(){
@@ -45,6 +59,7 @@ $("#appreciative").click(function(){
     $(".feelRelaxed").hide();
     $(".energeticAdvice").hide();
     $(".inspiredQuote").hide();
+    $(".randomQuote").text("");
 })
 
 $("#productive").click(function(){
@@ -53,5 +68,6 @@ $("#productive").click(function(){
     $(".feelRelaxed").hide();
     $(".energeticAdvice").hide();
     $(".inspiredQuote").hide();
+    $(".randomQuote").text("");
 })
 
